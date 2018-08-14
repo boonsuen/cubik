d5753e9658bc56ba6220ec60016ad36dbe46de36
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-08-14 20:20:15 +0800

    styling fix

--- src/containers/Post.js
+++ src/containers/Post.js
@@ -3,7 +3,10 @@ import { withRouteData, Link } from 'react-static';
 
 const Post = withRouteData(({ post }) => (
   <React.Fragment>
-    <Link to="/blog">{'<'} Back</Link>
+    <Link to="/blog" style={{
+      marginTop: '30px',
+      display: 'inline-block'
+    }}>{'<'} Back</Link>
     <br />
     <h3>{post.title}</h3>
     <p>{post.body}</p>
