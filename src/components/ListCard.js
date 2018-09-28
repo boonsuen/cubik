0c36381f716a248ff8bf61c35d694f6294f53814
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-09-28 17:59:35 +0800

    add icons for ListCard

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import LinkIcon from '../img/icons/link.svg';
+import GroupIcon from '../img/icons/group.svg';
+
 const StyledListCard = styled.div`
   width: 300px;
   height: 180px;
@@ -13,16 +16,32 @@ const StyledListCard = styled.div`
   flex-direction: column;
   box-sizing: border-box;
   position: relative;
+  color: #022773;
 
   h3 {
-    color: #022773;
     font-weight: 600;
     font-size: 24px;
-    margin: 20px 0;
+    margin: 15px 0;
+  }
+`;
+
+const ListCardStats = styled.div`
+  display: flex;
+
+  span {
+    display: inline-flex;
+    align-items: flex-end;
+    margin-right: 12px;
+    height: 28px;
   }
 
-  span:last-child {
-    margin-top: auto;
+  span:first-child img {
+    margin-bottom: -2px;
+  }
+
+  span:last-child img {
+    margin-right: 6px;
+    margin-bottom: 4px;
   }
 `;
 
@@ -39,7 +58,10 @@ const Underlying = styled.div`
 
 const ListCard = ({ title }) => (
   <StyledListCard>
-    <span>30 links, 5 groups</span>
+    <ListCardStats>
+      <span><img src={LinkIcon} />30 links</span>
+      <span><img src={GroupIcon} />5 groups</span>
+    </ListCardStats>
     <h3>{title}</h3>
     <span>Last updated on 29th Sep 18</span>
     <Underlying />
