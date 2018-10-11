3526f6bfd956a659f22a68c6d2af01bb76012cfa
Boonsuen Oh <ohboonsuen123@gmail.com>
2018-10-11 22:09:23 +0800

    turn 404.js into SFC

--- src/containers/404.js
+++ src/containers/404.js
@@ -1,32 +1,10 @@
 import React from 'react';
 
-export default class NotFound extends React.Component {
-  state = {
-    ready: false,
-  }
-  componentDidMount () {
-    this.makeReady()
-  }
-  makeReady = () => {
-    if (!this.state.ready) {
-      this.setState({
-        ready: true,
-      })
-    }
-  }
-  render () {
-    // return this.state.ready ? (
-    //   <React.Fragment>
-    //     <h1>NOT FOUND</h1>
-    //     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    //   </React.Fragment>
-    // ) : null
+const NotFound = () => (
+  <React.Fragment>
+    <h1>NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  </React.Fragment>
+);
 
-    return (
-      <React.Fragment>
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </React.Fragment>
-    )
-  }
-}
+export default NotFound;
