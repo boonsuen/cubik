d35ceb0e395a245da88e34943ea82229925c2d99
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-01-02 22:13:18 +0800

    HTTP getTitleOfUrl function

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,17 @@
 const functions = require('firebase-functions');
-
+const urlMetadata = require('url-metadata');
 const admin = require('firebase-admin');
+
 admin.initializeApp();
 
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref.toString());
-  });
+exports.getTitleOfUrl = functions.https.onRequest((req, res) => {
+  const url = req.query.url;
+  urlMetadata(url)
+    .then(metadata => {
+      console.log(metadata);
+      res.status(200).send(metadata.title);
+    }, error => {
+      console.log(error);
+      res.status(500).send(metadata.title);
+    });
 });
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
