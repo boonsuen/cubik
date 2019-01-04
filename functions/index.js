24912694a80ecfec616cf30679109214e1a3a95d
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-01-04 16:05:09 +0800

    Add meta fetch

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require('firebase-functions');
 const urlMetadata = require('url-metadata');
 const admin = require('firebase-admin');
-const metafetch = require('metafetch');
+const metaFetch = require('./metaFetch');
 
 const cors = require('cors')({
   origin: true,
@@ -12,14 +12,19 @@ admin.initializeApp();
 exports.getTitleOfUrl = functions.https.onRequest((req, res) => {   
   cors(req, res, () => {
     const url = req.query.url;
-    
+    metaFetch.fetch(url).then(meta => {
+      console.log('title: ', meta.title);
+      
+    }).catch(error => {
+      console.log(error);
+    });
     urlMetadata(url)
       .then(metadata => {
         // console.log(metadata);
         res.status(200).send(metadata.title);
       }, error => {
         console.log(error);
-        res.status(500).send(metadata.title);
+        res.status(500).send(error);
       });
   });
 });
