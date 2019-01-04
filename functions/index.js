44181d899fe72cdcd486b5d5ce61a45ca2e2ad42
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-01-04 16:45:30 +0800

    Use urlMetadata

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,16 @@
 const functions = require('firebase-functions');
-const urlMetadata = require('url-metadata');
+const urlMetadata = require('./urlMetadata');
 const admin = require('firebase-admin');
-const metaFetch = require('./metaFetch');
+
+admin.initializeApp();
 
 const cors = require('cors')({
   origin: true,
 });
 
-admin.initializeApp();
-
 exports.getTitleOfUrl = functions.https.onRequest((req, res) => {   
   cors(req, res, () => {
     const url = req.query.url;
-    metaFetch.fetch(url).then(meta => {
-      console.log('title: ', meta.title);
-      
-    }).catch(error => {
-      console.log(error);
-    });
     urlMetadata(url)
       .then(metadata => {
         // console.log(metadata);
