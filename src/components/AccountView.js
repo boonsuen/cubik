950a57d257c30f2c49e9827ec07877cabb4ba62b
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-01-07 23:47:21 +0800

    Start AccountView.js

---
+++ src/components/AccountView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AccountView extends React.Component {
+  render() {
+    return (
+      <header>
+        <div>Go back</div>
+        <h1>Account Settings</h1>
+      </header>
+    );
+  }
+}
+
+export default AccountView;
